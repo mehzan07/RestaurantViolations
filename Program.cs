c08c18f31eefc7de40de1194989f41edda3bb9ef
Mehrdad <mehzan07@yahoo.com>
2023-04-27 15:47:27 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+using static RestaurantViolations.RestaurantViolationsPrediction;
+
+// Create sample data
+ModelInput input = new ModelInput
+{
+    InspectionType = "Complaint",
+    ViolationDescription = "Inadequate sewage or wastewater disposal"
+};
+
+// Make prediction
+ModelOutput result = Predict(input);
+
+// Print Prediction
+Console.WriteLine($"Inspection type: {input.InspectionType}");
+Console.WriteLine($"Violation description: {input.ViolationDescription}");
+Console.WriteLine($"Predicted risk category: {result.PredictedLabel}");
+Console.ReadKey();
